0415b8a0b32bd438cf8fc3b143a60f7ef0448636
Sam Seltzer-Johnston <sam@sbseltzer.net>
2024-12-29 22:17:08 -0500

    More comprehensive comparison tests

--- Source/VersionMacros/Private/VersionMacros.cpp
+++ Source/VersionMacros/Private/VersionMacros.cpp
@@ -1,3 +1,23 @@
+/**
+* Version Macros for supporting projects that span multiple versions of Unreal.
+ * https://github.com/sbseltzer/VersionMacros
+ * 
+ * Copyright Sam Seltzer-Johnston 2024. All Rights Reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "VersionMacros.h"
 #include "Modules/ModuleManager.h"
 
@@ -5,23 +25,130 @@
 
 // Compile-time sanity tests
 #ifndef UE_VERSION_COMPILE_TIME_TESTS
-#define UE_VERSION_COMPILE_TIME_TESTS 0
+#define UE_VERSION_COMPILE_TIME_TESTS 1
 #endif
 
 #if UE_VERSION_COMPILE_TIME_TESTS
 #define compile_time_assert(b) static_assert((b), "Compile time assertion failed: "#b)
 
+//==============================================================================================
 // Test VersionMacros.h
 // If any of the following asserts fail, it means something is wrong with VersionMacros.h
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,0, ==, 5,0,0));
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,0,0, <, 5,0,0));
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,0, >=, 4,0,0));
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,1,20, <, 4,2,0));
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,3, >, 5,1,2));
-compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,3, <, 5,0,2));
+//==============================================================================================
 
+// EQ Comparisons
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,5,1, ==, 5,5,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, ==, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, ==, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, ==, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, ==, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, ==, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, ==, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, ==, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, ==, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, ==, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, ==, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, ==, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, ==, 4,21,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,20,3, ==, 4,20,3));
+
+// GTE Comparisons (Equal)
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,5,1, >=, 5,5,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, >=, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, >=, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, >=, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, >=, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, >=, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, >=, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, >=, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, >=, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, >=, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, >=, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, >=, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, >=, 4,21,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,20,3, >=, 4,20,3));
+
+// LTE Comparisons (Equal)
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,5,1, <=, 5,5,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, <=, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, <=, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, <=, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, <=, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, <=, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, <=, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, <=, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, <=, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, <=, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, <=, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, <=, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, <=, 4,21,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,20,3, <=, 4,20,3));
+
+// GT Comparisons
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,5,1, >, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, >, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, >, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, >, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, >, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, >, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, >, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, >, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, >, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, >, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, >, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, >, 4,21,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, >, 4,20,3));
+
+// GTE Comparisons (Greater)
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,5,1, >=, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, >=, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, >=, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, >=, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, >=, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, >=, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, >=, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, >=, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, >=, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, >=, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, >=, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, >=, 4,21,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, >=, 4,20,3));
+
+// LT Comparisons
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, <, 5,5,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, <, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, <, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, <, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, <, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, <, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, <, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, <, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, <, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, <, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, <, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, <, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,20,3, <, 4,21,2));
+
+// LTE Comparisons (Lesser)
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,4,4, <=, 5,5,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,3,2, <=, 5,4,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,2,1, <=, 5,3,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,1,1, <=, 5,2,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(5,0,3, <=, 5,1,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,27,2, <=, 5,0,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,26,2, <=, 4,27,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,25,4, <=, 4,26,2));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,24,3, <=, 4,25,4));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,23,1, <=, 4,24,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,22,3, <=, 4,23,1));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,21,2, <=, 4,22,3));
+compile_time_assert(UE_VERSION_COMPARE_EXPLICIT(4,20,3, <=, 4,21,2));
+
+//==============================================================================================
 // Test Prebuild Scripts
 // If any of the following asserts fail, it means something is wrong with the prebuild scripts
+//==============================================================================================
+
 #define UE_5_0_OR_LATER UE_VERSION_MINIMUM(5, 0)
 #if 1 // UE_5_0_OR_LATER
 compile_time_assert(UE_5_0_OR_LATER);
@@ -33,6 +160,7 @@ compile_time_assert(!UE_5_0_OR_LATER);
 #else
 compile_time_assert(UE_5_0_OR_LATER);
 #endif
+
 #define UE_5_4_OR_LATER UE_VERSION_MINIMUM(5, 4)
 #if 1 // UE_5_4_OR_LATER
 compile_time_assert(UE_5_4_OR_LATER);
@@ -44,6 +172,7 @@ compile_time_assert(!UE_5_4_OR_LATER);
 #else
 compile_time_assert(UE_5_4_OR_LATER);
 #endif
+
 #define TEST_MACRO UE_VERSION_MAXIMUM(5, 0)
 #if 0 // TEST_MACRO
 compile_time_assert(TEST_MACRO);

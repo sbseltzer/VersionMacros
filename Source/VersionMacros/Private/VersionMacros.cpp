31531c13a7bd73f627a516bf14e1036ddcfd67dd
Sam Seltzer-Johnston <sam@sbseltzer.net>
2025-01-01 00:03:14 -0500

    Happy New Year!

--- Source/VersionMacros/Private/VersionMacros.cpp
+++ Source/VersionMacros/Private/VersionMacros.cpp
@@ -2,7 +2,7 @@
 * Version Macros for supporting projects that span multiple versions of Unreal.
  * https://github.com/sbseltzer/VersionMacros
  * 
- * Copyright Sam Seltzer-Johnston 2024. All Rights Reserved.
+ * Copyright Sam Seltzer-Johnston 2025. All Rights Reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the

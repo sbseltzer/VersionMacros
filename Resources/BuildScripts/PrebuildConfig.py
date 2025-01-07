2789f191e60b8f8b9b58a3387fbf2443fc00f694
Sam Seltzer-Johnston <sam@sbseltzer.net>
2025-01-07 10:10:23 -0500

    Added more versions to the VersionsList and a note to users about removing unused ones.

--- Resources/BuildScripts/PrebuildConfig.py
+++ Resources/BuildScripts/PrebuildConfig.py
@@ -33,10 +33,11 @@ AllowVersionOrLaterReplacements = True
 # Whether to do text replacements for lines of the form "#if <1 or 0> // UE_<MajorVersion>_<MinorVersion>_OR_EARLIER"
 AllowVersionOrEarlierReplacements = True
 # Default file patterns to perform replacements in
-DefaultMatchFiles = [r'.*\.cpp', r'.*\.h', r'.*\.hpp', r'.*\.c', r'.*\.inl']
+DefaultMatchFiles = [r'.*\.h']
 # Generate fake macro text replacement configurations for UE_*_OR_LATER & UE_*_OR_EARLIER macros
 if AllowVersionOrLaterReplacements or AllowVersionOrEarlierReplacements:
-    VersionsList = ["4.17", "4.18", "4.19", "4.20", "4.21", "4.22", "4.23", "4.24", "4.25", "4.26", "4.27",
+    # Feel free to remove versions you don't intend to support from this list to improve prebuild performance
+    VersionsList = ["4.10", "4.11", "4.12", "4.13", "4.14", "4.15", "4.16", "4.17", "4.18", "4.19", "4.20", "4.21", "4.22", "4.23", "4.24", "4.25", "4.26", "4.27",
                     "5.0", "5.1", "5.2", "5.3", "5.4", "5.5"]
     for version in VersionsList:
         [major, minor] = split(r'\.', version, 1)

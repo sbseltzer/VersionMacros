bbb9ac855adf985a9597cb35bb85ad6c1c61f10d
Sam Seltzer-Johnston <sam@sbseltzer.net>
2025-01-07 10:05:38 -0500

    Fixed VersionMacros.Build.cs for pre-4.17 Unreal versions and verified it works in 4.12, 4.13, 4.16, and 4.17.

--- Source/VersionMacros/VersionMacros.Build.cs
+++ Source/VersionMacros/VersionMacros.Build.cs
@@ -4,16 +4,18 @@ using UnrealBuildTool;
 
 public class VersionMacros : ModuleRules
 {
+#if UE_4_17_OR_LATER
 	public VersionMacros(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
+		PublicDependencyModuleNames.Add("Core");
 	}
+#else
+	// NOTE: This is flagged as obsolete in 4.16 but the UE_*_OR_LATER C# macros weren't introduced until 4.17
+	public VersionMacros(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.Add("Core");
+	}
+#endif
 }
+

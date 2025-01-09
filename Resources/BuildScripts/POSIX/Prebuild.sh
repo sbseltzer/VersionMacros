8918ffcbef8b3e99bb37cbac8f6d68befad36e7f
Sam Seltzer-Johnston <sam@sbseltzer.net>
2025-01-08 22:20:13 -0500

    Merged Mac/Linux prebuild files into POSIX implementation. Ported the rest of the Win64 Python path logic, though it's currently untested.

---
+++ Resources/BuildScripts/POSIX/Prebuild.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+BuildVersionFile="$EngineDir/Build/Build.version"
+MajorVersion=$(< "$BuildVersionFile" grep -o '"MajorVersion".*,' | grep -o '\d*')
+MinorVersion=$(< "$BuildVersionFile" grep -o '"MinorVersion".*,' | grep -o '\d*')
+# Execute Prebuild.py using the Python binary that ships with Unreal Editor.
+PythonVersion=""
+if [ "$MajorVersion" -gt "4" ] || [ "$MinorVersion" -ge "26" ]; then
+  PythonVersion="3"
+fi
+# Per-version Python executable paths
+if [ "$MajorVersion" -gt "4" ] || [ "$MinorVersion" -ge "22" ]; then
+  PythonExe="$EngineDir/Binaries/ThirdParty/Python$PythonVersion/$HostPlatform/bin/python$PythonVersion"
+elif [ "$MajorVersion" -gt "4" ] || [ "$MinorVersion" -ge "19" ]; then
+  PythonExe="$EngineDir/Source/ThirdParty/Python$PythonVersion/$HostPlatform/bin/python"
+elif [ "$MajorVersion" -gt "4" ] || [ "$MinorVersion" -ge "13" ]; then
+  PythonExe="$EngineDir/Extras/ThirdPartyNotUE/emsdk/$HostPlatform/python/2.7.5.3_64bit/bin/python"
+elif [ "$MajorVersion" -gt "4" ] || [ "$MinorVersion" -ge "9" ]; then
+  PythonExe="$EngineDir/Source/ThirdParty/HTML5/emsdk/$HostPlatform/python/2.7.5.3_64bit/bin/python"
+fi
+# Fall back to Python executable in environment PATH.
+if [ ! -f "$PythonExe" ]; then
+    PythonExe="python"
+fi
+$PythonExe "$PluginDir/Resources/BuildScripts/Prebuild.py"
--- Resources/BuildScripts/Win64/Prebuild.ps1
+++ Resources/BuildScripts/Win64/Prebuild.ps1
@@ -7,21 +7,22 @@ if ($MajorVersion -gt 4 -or $MinorVersion -ge 26)
 {
     $PythonVersion = "3"
 }
+# Per-version Python executable paths
 if ($MajorVersion -gt 4 -or $MinorVersion -ge 22)
 {
-    $PythonExe = "$Env:EngineDir\Binaries\ThirdParty\Python$PythonVersion\Win64\python.exe"
+    $PythonExe = "$Env:EngineDir\Binaries\ThirdParty\Python$PythonVersion\$Env:HostPlatform\python.exe"
 }
 elseif ($MajorVersion -eq 4 -and $MinorVersion -ge 19)
 {
-    $PythonExe = "$Env:EngineDir\Source\ThirdParty\Python$PythonVersion\Win64\python.exe"
+    $PythonExe = "$Env:EngineDir\Source\ThirdParty\Python$PythonVersion\$Env:HostPlatform\python.exe"
 }
 elseif ($MajorVersion -eq 4 -and $MinorVersion -ge 13)
 {
-    $PythonExe = "$Env:EngineDir\Extras\ThirdPartyNotUE\emsdk\Win64\python\2.7.5.3_64bit\python.exe"
+    $PythonExe = "$Env:EngineDir\Extras\ThirdPartyNotUE\emsdk\$Env:HostPlatform\python\2.7.5.3_64bit\python.exe"
 }
 elseif ($MajorVersion -eq 4 -and $MinorVersion -ge 9)
 {
-    $PythonExe = "$Env:EngineDir\Source\ThirdParty\HTML5\emsdk\Win64\python\2.7.5.3_64bit\python.exe"
+    $PythonExe = "$Env:EngineDir\Source\ThirdParty\HTML5\emsdk\$Env:HostPlatform\python\2.7.5.3_64bit\python.exe"
 }
 # Fall back to Python executable in environment PATH.
 if (-Not (Test-Path "$PythonExe" -PathType Leaf))

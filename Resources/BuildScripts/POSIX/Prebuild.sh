74cf8700b2667bec248983b6f0b585ad7d0e17f2
Sam Seltzer-Johnston <sam@sbseltzer.net>
2025-08-21 21:00:01 -0400

    More portable Python executable deduction for POSIX shells

--- Resources/BuildScripts/POSIX/Prebuild.sh
+++ Resources/BuildScripts/POSIX/Prebuild.sh
@@ -3,9 +3,9 @@ BuildVersionFile="$EngineDir/Build/Build.version"
 MajorVersion=$(< "$BuildVersionFile" grep -o '"MajorVersion".*,' | grep -o '\d*')
 MinorVersion=$(< "$BuildVersionFile" grep -o '"MinorVersion".*,' | grep -o '\d*')
 # Find the local Python installation, with a preference for Python3
-PythonExe=$(where python3)
+PythonExe=$(command -v python3)
 if [ ! -f "$PythonExe" ]; then
-    PythonExe=$(where python)
+    PythonExe=$(command -v python)
     if [ ! -f "$PythonExe" ]; then
         echo "Failed to find Python! Please be sure it's installed to execute prebuild scripts."
         exit 1
